8f3b6addbb08fa3fc841ba51cd02b484ec69289b
Michał Skwara <michal.skwara@stxnext.pl>
2022-10-10 16:20:37 +0200

    [MM-60] fix missingFiles zip name and content filenames

--- src/controllers/device/utils.ts
+++ src/controllers/device/utils.ts
@@ -1,14 +1,22 @@
 import { findIndex } from 'lodash';
+import { format } from 'date-fns';
 import { OperationType } from 'consts/enums';
-import { IOperation } from 'models/Operation';
+import { IPopulatedOperation } from 'models/Operation/types';
 
 const isOperationPresent = (
-  operations: IOperation[],
+  operations: IPopulatedOperation[],
   operationType: OperationType
 ) =>
   findIndex(operations, (operation) => operation.type === operationType) >= 0;
 
-const removeOperationsOnFile = (operations: IOperation[], fileId: string) =>
-  operations.filter((operation) => operation.file.toString() !== fileId);
+const removeOperationsOnFile = (
+  operations: IPopulatedOperation[],
+  fileId: string
+) => operations.filter((operation) => operation.file.toString() !== fileId);
 
-export { isOperationPresent, removeOperationsOnFile };
+const generateFilename = (deviceName: string) => {
+  const dateString = format(new Date(), 'dd-MM-yyyy-Hm');
+  return `music-${deviceName}-${dateString}.zip`;
+};
+
+export { isOperationPresent, removeOperationsOnFile, generateFilename };
